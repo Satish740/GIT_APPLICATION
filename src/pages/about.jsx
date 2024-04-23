aaa6dfa7d18925fc22af4d99397168d8dbc9605c
Satish7490 <jsatish053@gmail.com>
2024-04-23 18:55:01 -0500

    Added Linkedin and Whatsapp Link

--- src/pages/about.jsx
+++ src/pages/about.jsx
@@ -1,4 +1,4 @@
-import {FaLinkedin} from "react-icons/fa"
+import {FaLinkedin,FaWhatsappSquare} from "react-icons/fa"
 
 function About() {
     return (
@@ -14,11 +14,14 @@ function About() {
         </p>
         <p className='text-lg text-gray-400'>
             Created by{' '}
+            
           <a className='text-white' href='https://github.com/Satish740'>
             Satish Jagadish
           </a>
-          <a  href="https://www.linkedin.com/in/satish-jagadish-b2916a167/" target="_blank" rel="noreferrer"> <FaLinkedin /> </a>
-
+          <div className="flex justify-center ">
+          <a  href="https://www.linkedin.com/in/satish-jagadish-b2916a167/" target="_blank" rel="noreferrer" class="mr-2 text-2xl"> <FaLinkedin /> </a>
+          <a  href="http://api.whatsapp.com/send?phone=4146391388&text=Hello, I saw your github profile finder website. " target="_blank" rel="noreferrer" class=" text-2xl"> <FaWhatsappSquare /></a>
+          </div>
         </p>
       </div>
       </div>
